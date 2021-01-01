7382ff20d64239e971a9ee48f32989c5d25f3950
Abolfazl Ahmadi-Rahmat <38430894+ahmadiphy@users.noreply.github.com>
2021-01-01 14:47:06 +0330

    Update ern.cpp

--- src/ern.cpp
+++ src/ern.cpp
@@ -1,3 +1,4 @@
+//A class to generate ER random networks
 #include "ern.h"
 //
 using namespace::std;
@@ -8,18 +9,18 @@ void ERn::ERnetwork(double avgK, int nn, iMatrix &aa)
     mt19937 gen(rd());  // to seed mersenne twister.
     uniform_int_distribution<> distall(0,nn-1);
     double linkss;
-    linkss=double((avgK*nn)/2);
+    linkss = double((avgK*nn)/2);
     int links;
-    links=int(linkss);
-    int i=0;
-    while(i<links){
-        int r1=0,r2=0;
-        r1=distall(gen);
-        r2=distall(gen);
-        if(aa[r1][r2]==0 && r1 != r2)
+    links = int(linkss);
+    int i = 0;
+    while(i < links){
+        int r1 = 0, r2 = 0;
+        r1 = distall(gen);
+        r2 = distall(gen);
+        if(aa[r1][r2] == 0 && r1 != r2)
         {
-            aa[r1][r2]=1;
-            aa[r2][r1]=1;
+            aa[r1][r2] = 1;
+            aa[r2][r1] = 1;
             ++i;
         }
     }
