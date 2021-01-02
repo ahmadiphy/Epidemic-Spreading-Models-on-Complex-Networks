13cf5bb97447a3bd921dd12467abfd9561c0b7ec
Abolfazl Ahmadi-Rahmat <38430894+ahmadiphy@users.noreply.github.com>
2021-01-02 19:22:44 +0330

    Update si.cpp

--- src/si.cpp
+++ src/si.cpp
@@ -1,3 +1,4 @@
+//Simple SI spreading model to check network connectivity
 #include "si.h"
 using namespace::std;
 //
@@ -7,8 +8,8 @@ SI::SI()
 }
 void SI::ECheck_SI(int llnn, iMatrix &ac)
 {
-    Check_result=0;
-    int rr=llnn;
+    Check_result = 0;
+    int rr = llnn;
     random_device rd;
     mt19937 gen(rd());  // to seed mersenne twister.
     uniform_int_distribution<> distall(0,llnn-1);
@@ -17,47 +18,47 @@ void SI::ECheck_SI(int llnn, iMatrix &ac)
 #pragma omp parallel
     {
 #pragma omp for
-        for(int i=0;i<llnn;++i)
+        for(int i=0; i<llnn; ++i)
         {
-            dy1[i]=0;
+            dy1[i] = 0;
         }
     }
-    int c1=0;
-    c1=distall(gen);
-    dy1[c1]=1;
+    int c1 = 0;
+    c1 = distall(gen);
+    dy1[c1] = 1;
     infects.push_back(c1);
-    int iii=0;
-    for(int ii=0;ii<rr;++ii)
+    int iii = 0;
+    for(int ii=0; ii<rr; ++ii)
     {
-        int isize=infects.size();
-        for(int m=iii;m<isize;++m)
+        int isize = infects.size();
+        for(int m=iii; m<isize; ++m)
         {
-            int im=infects[m];
-            for(int j=0;j<ac[im].size();j++)
+            int im = infects[m];
+            for(int j=0; j<ac[im].size(); j++)
             {
-                int jc=ac[im][j];
-                if(dy1[jc]==0)
+                int jc = ac[im][j];
+                if(dy1[jc] == 0)
                 {
-                    dy1[jc]=1;
+                    dy1[jc] = 1;
                     infects.push_back(jc);
                 }
             }
         }
-        iii=isize-1;
+        iii = isize - 1;
     }
-    cout<<llnn<<endl;
-    cout<<"infs:"<<infects.size()<<"**"<<infects.capacity()<<endl;
-    if(llnn==infects.size())
-        Check_result=1;
+    cout << llnn << endl;
+    cout << "infs:" << infects.size() << "**" << infects.capacity() << endl;
+    if(llnn == infects.size())
+        Check_result = 1;
     else
-        Check_result=0;
-    int sum=0;
-    for(int i=0;i<llnn;++i)
+        Check_result = 0;
+    int sum = 0;
+    for(int i=0; i<llnn; ++i)
     {
-        if(dy1[i]==1)
+        if(dy1[i] == 1)
             sum++;
     }
-    cout<<sum<<endl;
+    cout << sum << endl;
 }
 //
 int SI::get_Check_result()
